2fe7c6ce08ad720e8c30bc312fd63c3b1bc67579
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2025-02-06 19:34:28 -0600

    Realizando la actividad de Campus

---
+++ IntroduccionAJavaScript/tipos-de-dato.js
@@ -0,0 +1,16 @@
+//se usa el typeof para saber el tipo de dato de un dato
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function () {});
+console.log(typeof "asda");
